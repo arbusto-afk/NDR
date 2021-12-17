9dd23344c974f813392a6f0779b88b8d7981f956
Arbusto <88610004+arbusto-afk@users.noreply.github.com>
2021-12-17 13:21:32 -0300

    Add files via upload
    Uploaded new updated source code

---
+++ NDR/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+
+namespace NDR
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public int ReturnAllowed = 250;
+        public int Radius = 10;
+        public int TpRadius;
+        public bool slow;
+        public float slowtime;
+        public void LoadDefaults()
+        {
+            ReturnAllowed = 250;
+            Radius = 10;
+            TpRadius = 14;
+            slow = true;
+            slowtime = 5;
+            
+        }
+    }
+}
